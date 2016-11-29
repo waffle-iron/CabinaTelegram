56ec1121ffd5df0bf3307bd57832b4c954624748
Jose Luis Salazar Gonzalez <joseluis25sg@gmail.com>
2016-11-29 15:48:51 +0100

    Add restrictions and some visual improvements to votations

--- votacion.py
+++ votacion.py
@@ -14,7 +14,7 @@ class Votacion:
         self.pide_titulo(message.chat.id)
 
     def pide_titulo(self, chat_id):
-        msg = bot.send_message(chat_id, '¿Qué título desea para la votación?')
+        msg = bot.send_message(chat_id, 'Dime un *título* para la votación:', parse_mode='Markdown')
         bot.register_next_step_handler(msg, self.captura_titulo)
 
     def captura_titulo(self, message):
@@ -22,14 +22,18 @@ class Votacion:
         chat_id = message.chat.id
 
         if telebot.util.is_command(titulo):
-            bot.send_message(message.chat.id, 'Votación cancelada')
+            bot.send_message(message.chat.id, '❌ Votación cancelada')
             return False
         else:
             self.titulo = titulo
             self.pide_pregunta(chat_id)
 
     def pide_pregunta(self, chat_id):
-        msg = bot.send_message(chat_id, 'Dime una pregunta, /done para terminar')
+        if self.get_num_preguntas() == 0:
+            text = '❔Dime tu primera *pregunta:*'
+        else:
+            text = '❔Siguiente *pregunta:* \n\nPara terminar escribe /done o simplemente pulsa sobre el enlace.'
+        msg = bot.send_message(chat_id, text, parse_mode='Markdown')
         bot.register_next_step_handler(msg, self.captura_pregunta)
 
     def captura_pregunta(self, message):
@@ -38,18 +42,29 @@ class Votacion:
 
         if telebot.util.is_command(pregunta):
             command = telebot.util.extract_command(pregunta)
-            if command == 'done':
-                bot.send_message(chat_id, 'De acuerdo, encuesta creada')
+            if command == 'done' and self.get_num_preguntas() >= 1:
+                bot.send_message(chat_id, '✅ Encuesta creada con éxito')
                 bot.send_message(chat_id, str(self.preguntas_respuestas))
+            elif command == 'done':
+                bot.send_message(chat_id, 'Necesitas al menos una pregunta para crear la votación.')
+                self.pide_pregunta(chat_id)
             else:
-                bot.send_message(chat_id, 'Votación cancelada')
+                bot.send_message(chat_id, '❌ Votación cancelada')
                 return False
         else:
             self.añade_pregunta(pregunta)
             self.pide_respuesta(message.chat.id)
 
     def pide_respuesta(self, chat_id):
-        msg = bot.send_message(chat_id, 'Dime una respuesta, /done para terminar')
+        if self.get_num_respuestas() == 0:
+            text = '✏️ Dime una *respuesta:*'
+        elif self.get_num_respuestas() == 1:
+            text = '✏️ Dime otra *respuesta:*'
+        else:
+            text = '✏️ Dime otra *respuesta:* \n\nPara pasar a la siguiente pregunta escribe /done' \
+                   ' o simplemente pulsa sobre el enlace.'
+
+        msg = bot.send_message(chat_id, text, parse_mode='Markdown')
         bot.register_next_step_handler(msg, self.captura_respuesta)
 
     def captura_respuesta(self, message):
@@ -58,10 +73,13 @@ class Votacion:
 
         if telebot.util.is_command(respuesta):
             command = telebot.util.extract_command(respuesta)
-            if command == 'done':
+            if command == 'done' and self.get_num_respuestas() >= 2:
                 self.pide_pregunta(chat_id)
+            elif command == 'done':
+                bot.send_message(chat_id, 'Necesitas al menos dos respuestas para la pregunta.')
+                self.pide_respuesta(chat_id)
             else:
-                bot.send_message(chat_id, 'Votación cancelada')
+                bot.send_message(chat_id, '❌ Votación cancelada')
                 return False
         else:
             self.añade_respuesta(respuesta)
@@ -74,3 +92,10 @@ class Votacion:
     def añade_respuesta(self, respuesta):
         pregunta = sorted(self.preguntas_respuestas)[-1]
         self.preguntas_respuestas[pregunta].append(respuesta)
+
+    def get_num_preguntas(self):
+        return len(self.preguntas_respuestas)
+
+    def get_num_respuestas(self):
+        pregunta = sorted(self.preguntas_respuestas)[-1]
+        return len(self.preguntas_respuestas[pregunta])
