5cfd1fd3773dec413b18702b46a34879a73f9737
Jorge Puente Zaro <jorgepuent@gmail.com>
2016-12-10 13:42:39 +0100

    Creacion de votacion, preguntas y respuestas e la base de datos.

--- utils.py
+++ utils.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 import os
 import sqlite3 as lite
-import sys
 
 class Utils:
-    def almacenar_encuesta(titulo,preguntas_respuestas):
+    def almacenar_votacion(self,titulo, preguntas_respuestas):
 
         #Ruta donde se creará la base de datos. En sistemas UNIX-like será /home/username/votacion.db
         home = os.path.expanduser('~')
@@ -17,22 +16,18 @@ class Utils:
         with con:
 
             cur = con.cursor()
-
             #Activar el soporte de sqlite3 para claves foráneas
             cur.execute("PRAGMA foreign_keys = ON")
-
             #Insertar los datos en la BD:
-
             #Crear votación
-            cur.execute("INSERT INTO Votacion(Nombre) VALUES(%s)",titulo)
-            votacionId = int(cur.execute("SELECT Votacion.id FROM Votacion WHERE Votacion.titulo == %s",titulo))
+            cur.execute("INSERT INTO Votacion(Nombre) VALUES(?)",titulo)
+            votacionId = int(cur.execute("SELECT Votacion.id FROM Votacion WHERE Votacion.titulo == ?",titulo))
 
-                       # Crear preguntas para esa votación
-        for pregunta in preguntas_respuestas:
-            cur.execute("INSERT INTO Pregunta(Texto,Max_respuestas) VALUES(%s,%d)", preguntas_respuestas[pregunta], 50)
-            preguntaId = int(cur.execute("SELECT Pregunta.id FROM Pregunta WHERE Pregunta.texto == %s",
-                                         preguntas_respuestas[pregunta]))
-            for pregunta, respuesta in preguntas_respuestas:
-                cur.execute("INSERT INTO RESPUESTA(Text,Id_pregunta) VALUES(%s,%d)", respuesta, preguntaId)
-
+            # Crear preguntas para esa votación
+            for pregunta in preguntas_respuestas:
+                cur.execute("INSERT INTO Pregunta(Texto,Max_respuestas,Id_votacion) VALUES(?,?,?)", pregunta, 50,votacionId)
+                preguntaId = int(cur.execute("SELECT Pregunta.id FROM Pregunta WHERE Pregunta.texto == ?", pregunta))
+                #Almacenar respuestas de
+                for respuesta in preguntas_respuestas[pregunta]:
+                    cur.execute("INSERT INTO RESPUESTA(Text,Veces_elegida,Id_pregunta) VALUES(?,?,?)", respuesta, 0, preguntaId)
 
